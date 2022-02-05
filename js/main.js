c1e53fc126d45f90b885869af6a7377830fb9b01
Alan <azzheng@umass.edu>
2022-02-05 17:33:41 -0500

    Added the layout

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const add = function(a, b) {
+
+}
+
+const subtract = function(a, b) {
+
+}
+
+const multiply = function(arr) {
+
+}
+
+const divide = function(a, b) {
+
+}
+
+const operate = function(a, b, op) {
+
+}
+
+//create the 9 buttons
