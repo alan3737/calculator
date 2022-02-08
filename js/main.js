9a8c90f0903cf32d15a7f3839bd5a400c7bccf19
Alan <azzheng@umass.edu>
2022-02-07 22:27:51 -0500

    Made some changes to js

--- js/main.js
+++ js/main.js
@@ -30,6 +30,42 @@ const operate = function(a, b, op) {
     }
 }
 
+const evaluate = function (arr) {
+    let total = 0;
+    let value = 0;
+    let op = null;
+    let dec = 0;
+    let neg = 1;
+    arr.forEach(function(n) {
+        if (n === "-") {
+            neg = -1;
+        } else if (n === ".") {
+            dec = 10;
+        } else if (!isNaN(n)) {
+            if (dec > 0) {
+                value = (value + (n / dec)) * neg;
+                dec *= 10;
+                neg = 1;
+            } else {
+                value = (value * 10 + n) * neg;
+                neg = 1;
+            }
+        } else  {
+            if(total === 0) {
+                total = value;
+            } else {
+                total = operate(total, value, op);
+            }        
+            value = 0;
+            op = n;
+            dec = 0;
+        }
+    });
+    let solu = operate(total, value, op);
+    screen.textContent = solu;
+}
+
+
 const compute = function (e) {
     if (this.textContent === "back") {
         arr.pop();
@@ -38,34 +74,7 @@ const compute = function (e) {
         arr = [];
         screen.textContent = 0;
     }  else if (this.textContent === "=") {
-        let total = 0;
-        let value = 0;
-        let op = null;
-        let dec = 0;
-        arr.forEach(function(n) {
-            if (n === ".") {
-                dec = 10;
-            } else if (!isNaN(n)) {
-                if (dec > 0) {
-                    value = value + (n / dec);
-                    dec *= 10;
-                } else {
-                    value = value * 10 + n;
-                }
-            } else  {
-                if(total === 0) {
-                    total = value;
-                } else {
-                    total = operate(total, value, op);
-                }        
-                value = 0;
-                op = n;
-                dec = 0;
-            }
-        });
-        let solu = operate(total, value, op);
-        screen.textContent = solu;
-        arr = [solu];
+        arr = [evaluate(arr)];
     } else if(this.textContent === "." || !isNaN(this.textContent)) {
         if (screen.textContent === "0" || isNaN(screen.textContent)) {
             screen.textContent = "";
