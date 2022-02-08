b45293ad2077c3174c6c20c1d22187027cf2eb26
Alan <azzheng@umass.edu>
2022-02-08 00:13:31 -0500

    Changed the method to a simpler algo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+let prev = 0;
+let cur = 0;
+let op = 0;
+
 const add = function(a, b) {
     return a + b;
 }
@@ -30,64 +34,43 @@ const operate = function(a, b, op) {
     }
 }
 
-const evaluate = function (arr) {
-    let total = 0;
-    let value = 0;
-    let op = null;
-    let dec = 0;
-    let neg = 1;
-    arr.forEach(function(n) {
-        if (n === "-") {
-            neg = -1;
-        } else if (n === ".") {
-            dec = 10;
-        } else if (!isNaN(n)) {
-            if (dec > 0) {
-                value = (value + (n / dec)) * neg;
-                dec *= 10;
-                neg = 1;
-            } else {
-                value = (value * 10 + n) * neg;
-                neg = 1;
-            }
-        } else  {
-            if(total === 0) {
-                total = value;
-            } else {
-                total = operate(total, value, op);
-            }        
-            value = 0;
-            op = n;
-            dec = 0;
-        }
-    });
-    let solu = operate(total, value, op);
-    screen.textContent = solu;
-}
 
 
 const compute = function (e) {
     if (this.textContent === "back") {
-        arr.pop();
+        cur = Math.floor(cur / 10);
         screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
     } else if (this.textContent === "clear") {
-        arr = [];
         screen.textContent = 0;
-    }  else if (this.textContent === "=") {
-        arr = [evaluate(arr)];
-    } else if(this.textContent === "." || !isNaN(this.textContent)) {
-        if (screen.textContent === "0" || isNaN(screen.textContent)) {
-            screen.textContent = "";
-        } 
-        screen.textContent += this.textContent;
-        arr.push(+this.textContent);
+        prev = 0;
+        cur = 0;
+        op = 0;
+    }  else if (this.textContent === "=" && op != 0 && cur != 0) {
+        prev = operate(prev, cur, op);
+        cur = 0;
+        op = 0;
+        screen.textContent = prev;
+    } else if (this.textContent === "." && op === 0) {
+        // do decimal and finished
+        screen.textContent += ".";
+    } else if (!isNaN(this.textContent)) {
+        if (cur === 0 || isNaN(screen.textContent) ) {
+            screen.textContent = this.textContent;
+        } else {
+            screen.textContent += this.textContent;
+        }
+        cur = cur * 10 + (+this.textContent);
     } else {
-        arr.push(this.textContent);
+        if (prev === 0) {
+            prev = cur;
+        }
+        op = this.textContent;
+        cur = 0;
         screen.textContent = this.textContent;
     }
+    console.log(prev, cur, op);
 }
 
-let arr = []
 const buttons = document.querySelectorAll("button");
 const screen = document.querySelector(".screen");
 buttons.forEach(button => button.addEventListener('click', compute));
